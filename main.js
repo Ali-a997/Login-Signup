dd123d40af9584b85d1b214bace145984b20fb16
Ali-a997 <alialaji81@gmail.com>
2025-01-11 23:00:27 +0800

    Add files via upload
    Uploded java

---
+++ main.js
@@ -0,0 +1,21 @@
+const navbarMenu = document.querySelector(".navbar .links");
+const showPopupBtn = document.querySelector(".login-btn");
+const hidePopupBtn = document.querySelector(".form-popup .close-btn");
+const menuBtn = document.querySelector(".form-popup .close-btn");
+const formPopup = document.querySelector(".form-popup");
+const loginSignupLink = document.querySelectorAll(".form-box .bottom-link a")
+
+menuBtn.addEventListener("click", () =>{
+      navbarMenu.classList.toggle("show-menu")
+})
+showPopupBtn.addEventListener("click", ()=>{
+    document.body.classList.toggle("show-popup");
+});
+hidePopupBtn.addEventListener("click",()=> showPopupBtn.click());
+loginSignupLink.forEach(link =>{
+    link.addEventListener("click", (e) =>{
+        e.preventDefault();
+        formPopup.classList  [link.id === "signup-link" ? 'add' : 'remove']("show-signup");
+
+    });
+})
